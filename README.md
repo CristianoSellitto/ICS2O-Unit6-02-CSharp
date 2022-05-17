911d5e440529f92a9f40ae9c4ebc2f8e9ba5ffc0
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-05-17 17:11:32 +0000

    Added program

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# ICS2O-Unit#-##-CSharp
+# ICS2O-Unit6-02-CSharp
 
-[![GitHub's Super Linter](https://github.com/CristianoSellitto/<REPOSITORY>/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/CristianoSellitto/<REPOSITORY>/actions)
+[![GitHub's Super Linter](https://github.com/CristianoSellitto/ICS2O-Unit6-02-CSharp/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/CristianoSellitto/ICS2O-Unit6-02-CSharp/actions)
 
-[![Run on Repl.it](https://repl.it/badge/github/CristianoSellitto/<REPOSITORY>)](https://repl.it/github/CristianoSellitto/<REPOSITORY>)
+[![Run on Repl.it](https://repl.it/badge/github/CristianoSellitto/ICS2O-Unit6-02-CSharp)](https://repl.it/github/CristianoSellitto/ICS2O-Unit6-02-CSharp)
--- main.cs
+++ main.cs
@@ -1,14 +1,31 @@
 // Cristiano
-// ICS2O-Unit0-00-CSharp
-// Date 2022
+// ICS2O-Unit6-02-CSharp
+// May 17 2022
 
 using System;
+using System.IO;
 
 class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World");
+        if (File.Exists("text.txt") == false)
+        {
+            File.WriteAllText("text.txt", "0");
+        }
+        string fileText = File.ReadAllText("text.txt");;
+        fileText = Convert.ToString(Convert.ToInt32(fileText) + 1);
+        File.WriteAllText("text.txt", fileText);
+        if (Convert.ToInt32(fileText) == 1)
+        {
+            Console.WriteLine("You’ve opened this program 1 time.");
+        }
+        else
+        {
+            Console.WriteLine("You’ve opened this program " + fileText + " times.");
+        }
+        Console.WriteLine("\nPress any key to quit...");
+        Console.ReadKey();
         Console.WriteLine("\nFinished.");
     }
 }
